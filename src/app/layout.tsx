eebf02dcb9e3525f9f61a405df0072d54e44d7b2
Chung <arguello.chung@gmail.com>
2025-01-11 15:04:01 -0500

    single page lgtb website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BreadBanner from "@/components/Banner";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <BreadBanner />
+        <div className="container mx-auto">
+          {children}
+        </div>
+
       </body>
     </html>
   );
