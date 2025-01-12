f2a9ec52434f89953e5b6fa3d87c089b948d31fe
Chung <arguello.chung@gmail.com>
2025-01-11 19:39:16 -0500

    change import name to match export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BreadBanner from "@/components/Banner";
+import Banner from "@/components/Banner";
 
 
 const geistSans = Geist({
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <BreadBanner />
+        <Banner />
         <div className="container mx-auto">
           {children}
         </div>
